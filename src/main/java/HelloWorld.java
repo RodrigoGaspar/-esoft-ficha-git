b6884d4bfefd188624af78d8a03a9369d5ec8428
drcor <53583855+drcor@users.noreply.github.com>
2024-02-29 10:10:46 +0000

    adicionar o meu nome

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,6 +2,8 @@
     import javax.swing.*;
     public class HelloWorld extends JFrame {
         private JPanel panel1;
+        private JLabel lblMensagem2;
+
         public HelloWorld(){
             setContentPane(panel1);
             pack();
